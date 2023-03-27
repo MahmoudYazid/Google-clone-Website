c5e9a006802f0a312813a4f283b7fbee9d69dc71
myaz <myaz>
2023-03-27 04:59:17 +0200

    new change for mobile browsers
    new change for mobile browsers

--- src/Footer.js
+++ src/Footer.js
@@ -2,17 +2,14 @@ import React from 'react'
 import './footer.css'
 export default function Footer() {
   return (
-    <div className='FooterDiv'>
-      <div className="footerOne">
-        <p className="FooterOneText">Egypt</p>
-      </div>
-
+    <div className="FooterDiv">
       <div className="footerTwo">
+        
         <div className="TextContainerOne">
           <p className="TextContainerOneTxt">About </p>
           <p className="TextContainerOneTxt">Advertising </p>
           <p className="TextContainerOneTxt">Business </p>
-          <div className='SearchWorkWordClass'>
+          <div className="SearchWorkWordClass">
             <p className="TextContainerOneTxt">How Search works </p>
           </div>
         </div>
